7ab495ca096805596361ba0a5bd84e8c08e45624
Yoram Michaeli <yorammi@yorammi.com>
2021-08-16 00:20:01 +0300

    Create ajEnv.groovy

---
+++ vars/ajEnv.groovy
@@ -0,0 +1,38 @@
+#!/usr/bin/groovy
+
+def initEnv(Map config=null) {
+    if (config == null) {
+        config = [:]
+    }
+    if (config.deleteWorkspace == null) {
+        config.deleteWorkspace = true
+    }
+
+    if(config.deleteWorkspace)
+    {
+        deleteWorkspace()
+    }
+    tciDefaults.initDefaults()
+    tciBuildUser.initBuildUserEnvVars()
+    if(tciBuildUser.buildStartedByUser()) {
+        def buildUserDisplayName = tciBuildUser.getBuildUserDisplayName()
+        currentBuild.displayName += " {${buildUserDisplayName}}"
+    }
+}
+
+def deleteWorkspace() {
+    step([$class: 'WsCleanup'])
+}
+
+def deleteWorkspaceAsRoot() {
+
+    try {
+        docker.image("library/ubuntu").inside('-u root') {
+            sh ("find ${WORKSPACE} -mindepth 1 -delete > /dev/null | true")
+            sh ("chmod -R 777 ${WORKSPACE} > /dev/null | true")
+        }
+    }
+    catch(error) {
+
+    }
+}
